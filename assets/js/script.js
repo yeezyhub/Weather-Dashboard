b1da64f9ec771a7a0689253fd57af9bb5ce71cc1
Yigit Ocak <yigitocak@Yigits-MBP.lan>
2022-10-21 18:53:17 -0500

    CSS/HTML/JS files updated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,11 @@ const iconEl = document.querySelector('.icon');
 const tempEl = document.querySelector('.temp');
 const windEl = document.querySelector('.wind');
 const humidityEl = document.querySelector('.humidity');
+const forecastEl = document.querySelector('#forecast');
 
 function kelvinToFahrenheit(kelvin) {
    let fahrenheit = 1.8 * (kelvin-273) + 32;
+   fahrenheit = fahrenheit.toFixed(2);
    return fahrenheit;
 }
 
@@ -64,45 +66,65 @@ function weatherApi(lat, lon, cityName, date, weatherCondition, temp, wind, humi
     humidityEl.textContent = 'Humidity: ' + humidity + ' %';
     iconEl.setAttribute('src', `http://openweathermap.org/img/wn/${weatherCondition}@2x.png`);
     iconEl.classList.remove('hide');
+    cityInfo.classList.remove('hide');
 
-    let firstDay = data.list[0];
-    let secondDay = data.list[8];
-    let thirdDay = data.list[16];
-    let fourthDay = data.list[24];
-    let fifthDay = data.list[32];
+for (let i = 0; i < data.list.length; i++) {
+    let condition = data.list[i].dt_txt.includes("09:00:00");
+        if (condition) {
+            console.log(data.list[i]);
+            let forecastContainer = document.createElement('div');
+            forecastContainer.classList.add("card")
+            let forecastCard = `
+            <div class="card-body">
+              <h5 class="card-title">${data.list[i].dt_txt}</h5>
+              <img src='http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png' class="card-subtitle mb-2 text-muted"></img>
+              <p class="card-text">Temp: ${kelvinToFahrenheit(data.list[i].main.temp)} \u00B0F</p>
+              <p class="card-text">Wind: ${data.list[i].wind.speed} MPH</p>
+              <p class="card-text">Humidity: ${data.list[i].main.humidity} %</p>
+            </div>
+            `;
+            forecastContainer.innerHTML = forecastCard;
+            forecastEl.appendChild(forecastContainer);
+        }
+}
+    // let firstDay = data.list[1];
+    // let secondDay = data.list[9];
+    // let thirdDay = data.list[17];
+    // let fourthDay = data.list[25];
+    // let fifthDay = data.list[33];
 
-    let firstDayTemp = kelvinToFahrenheit(firstDay.main.temp);
-    let secondDayTemp = kelvinToFahrenheit(secondDay.main.temp);
-    let thirdDayTemp = kelvinToFahrenheit(thirdDay.main.temp);
-    let fourthDayTemp = kelvinToFahrenheit(fourthDay.main.temp);
-    let fifthDayTemp = kelvinToFahrenheit(fifthDay.main.temp);
+    // console.log(firstDay);
+    // console.log(secondDay);
+    // console.log(thirdDay);
+    // console.log(fourthDay);
+    // console.log(fifthDay);
 
-    let firstDayWind = firstDay.wind.speed;
-    let secondDayWind = secondDay.wind.speed;
-    let thirdDayWind = thirdDay.wind.speed;
-    let fourthDayWind = fourthDay.wind.speed;
-    let fifthDayWind = fifthDay.wind.speed;
-
-    let firstDayHum = firstDay.main.humidity;
-    let secondDayHum = secondDay.main.humidity;
-    let thirdDayHum = thirdDay.main.humidity;
-    let fourthDayHum = fourthDay.main.humidity;
-    let fifthDayHum = fifthDay.main.humidity;
-
-    
+    // let firstDayIcon = firstDay.weather[0].icon;
+    // let secondDayIcon =  secondDay.weather[0].icon;
+    // let thirdDayIcon = thirdDay.weather[0].icon;
+    // let fourthDayIcon = fourthDay.weather[0].icon;
+    // let fifthDayIcon = fifthDay.weather[0].icon;
 
 
-    console.log(firstDayHum);
+    // let firstDayTemp = kelvinToFahrenheit(firstDay.main.temp);
+    // let secondDayTemp = kelvinToFahrenheit(secondDay.main.temp);
+    // let thirdDayTemp = kelvinToFahrenheit(thirdDay.main.temp);
+    // let fourthDayTemp = kelvinToFahrenheit(fourthDay.main.temp);
+    // let fifthDayTemp = kelvinToFahrenheit(fifthDay.main.temp);
 
-    // for (let i = 0; i < array.length; i++) {
-        
-        
-    // }
+    // let firstDayWind = firstDay.wind.speed;
+    // let secondDayWind = secondDay.wind.speed;
+    // let thirdDayWind = thirdDay.wind.speed;
+    // let fourthDayWind = fourthDay.wind.speed;
+    // let fifthDayWind = fifthDay.wind.speed;
+
+    // let firstDayHum = firstDay.main.humidity;
+    // let secondDayHum = secondDay.main.humidity;
+    // let thirdDayHum = thirdDay.main.humidity;
+    // let fourthDayHum = fourthDay.main.humidity;
+    // let fifthDayHum = fifthDay.main.humidity;
 
 
-
-
-    console.log(kelvinToFahrenheit(firstDay.main.temp));
 
     })
 
@@ -115,28 +137,31 @@ let eachCity;
 let cityButton;
 let cityButtonEl;
 
-cityNames = Object.keys(localStorage);
-listCities();
+
 
 function search() {
     citiesLocal = localStorage.setItem(textBox.value, '');
+    cityList.textContent = '';
+    listCities();
     // textBox.value = ''; //resets the text in textbox
 
 }
 
 function listCities() {
+    cityNames = Object.keys(localStorage);
     for (let i = 0; i < localStorage.length; i++) {
         cityListLocal[i] = cityNames[i];
         cityButton = document.createElement('button');
         cityButtonEl = cityList.appendChild(cityButton);
+        // cityList.children.classList.add('addedButton');
+        // cityButtonEl.style.cssText = 'box-sizing: border-box';
         cityButtonEl.textContent = cityListLocal[i];
-   
    }
 }
 
-
-
+listCities();
 
 
 searchButton.addEventListener('click', search);
 searchButton.addEventListener('click', locationApi);
+
